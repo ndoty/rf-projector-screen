38076e91c40351e11aeb43e707252b60241af1d6
Nick Faust-Doty <n.faustdoty@gmail.com>
2018-03-19 22:18:42 -0400

    clean up

--- ProjectorScreen433mhz.ino
+++ ProjectorScreen433mhz.ino
@@ -14,16 +14,16 @@
 
 RCSwitch RF = RCSwitch();
 
-const byte ledPin = 13; //Using Pin #13 (D10) to turn on and off LED
-const byte mhzPin = 10; //Using Pin #10 (D7) to send 433mhz signals to screen
-const byte interruptPin = 2; //Nano only works on pin 2 (RX0) or 3 (RST) let's use 2 (RX0)
+const byte ledPin = 13; //Using Pin D13 to turn on and off LED
+const byte mhzPin = 10; //Using Pin D10 to send 433mhz signals to screen
+const byte interruptPin = 2; //Nano only works on pin D2 or D3 let's use D2
 const long lowerMhz = 2981764; //HomeGear Remote sent this command to lower so let's use it in the code
 const long raiseMhz = 2981761; //HomeGear Remote sent this command to raise so let's use it in the code
 const byte bitLength = 24; //HomeGear Remote used a 24 bit length so let's use it in the code
 const long pulseRate = 384; //HomeGear pulse rate was 384ms so let's use it in the code
 byte state = 2; // Set to 2 intitialy so both raise or lower will work on boot
 
-void setup() {  
+void setup() {
   RF.enableTransmit(mhzPin); //Using Pin #10 (D7) to send 433mhz signals to screen
   RF.setProtocol(1); //HomeGear Screen uses protocol 1 so set it
 
@@ -47,8 +47,6 @@ void move() {
 
   delay(1000); //We Read it now wait a second before we act
 
-  Serial.print(status);
-
   switch (status) {
     case LOW: //No Signal
       raiseScreen(); //Raise
@@ -60,12 +58,12 @@ void move() {
 }
 
 void raiseScreen() {
-  if (state != 0 ) { //Only run this once if we are already up
+  if (state != 0 ) { //Only run this once if we are already up. Nano runs functions multiple times for some reason
     digitalWrite(ledPin, HIGH); //Visual LED for signal
     RF.send(raiseMhz, bitLength); //Send Command
-    delay(pulseRate);
+    delay(1);
     RF.send(raiseMhz, bitLength); //Send again
-    delay(pulseRate);
+    delay(1);
     RF.send(raiseMhz, bitLength); //Send again
     digitalWrite(ledPin, LOW); //Turn off Visual LED for Signal
   }
@@ -73,7 +71,7 @@ void raiseScreen() {
 }
 
 void lowerScreen() {
-  if (state != 1) {  //Only run this once if we are already down
+  if (state != 1) {  //Only run this once if we are already down. Nano runs functions multiple times for some reason
     digitalWrite(ledPin, HIGH); //Visual LED for signal
     RF.send(lowerMhz, bitLength); //Send Command
     delay(pulseRate);
