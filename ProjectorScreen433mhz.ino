40100a8981cde17b7cb45831c0fda678e231c1f9
Nick Faust-Doty <n.faustdoty@gmail.com>
2018-03-19 23:52:50 -0400

    readme updates

--- ProjectorScreen433mhz.ino
+++ ProjectorScreen433mhz.ino
@@ -1,12 +1,15 @@
 /*
   Small program to catch a 12v signal from a projector
-  and lower a HomeGear Motorized Screen. 
+  and raise or lower a HomeGear Motorized Screen. 
   Values used were read from the HomeGear remote.
   Modify code to read the 433mhz signals and change
   accordingly for your screen commands.
   Arduino Nano was used.
   
+  Special thanks to sui77 for the RC Switch library
   https://github.com/sui77/rc-switch/
+  
+  This Repo
   https://github.com/ndoty/
 */
 
@@ -14,25 +17,29 @@
 
 RCSwitch RF = RCSwitch();
 
-const byte ledPin = 13; //Using Pin D13 to turn on and off LED
-const byte mhzPin = 10; //Using Pin D10 to send 433mhz signals to screen
-const byte interruptPin = 2; //Nano only works on pin D2 or D3 let's use D2
+// Configurable
 const long lowerMhz = 2981764; //HomeGear Remote sent this command to lower so let's use it in the code
 const long raiseMhz = 2981761; //HomeGear Remote sent this command to raise so let's use it in the code
 const byte bitLength = 24; //HomeGear Remote used a 24 bit length so let's use it in the code
 const long pulseRate = 384; //HomeGear pulse rate was 384ms so let's use it in the code
+const byte protocal = 1; //HomeGear protocol is 1 so let's use it in the code
+
+// Don't Modify unless you know what you are doing
+const byte ledPin = 13; //Using Pin D13 to turn on and off LED
+const byte mhzPin = 10; //Using Pin D10 to send 433mhz signals to screen
+const byte interruptPin = 2; //Nano only works on pin D2 or D3 let's use D2
 byte state = 2; // Set to 2 intitialy so both raise or lower will work on boot
 
 void setup() {
-  RF.enableTransmit(mhzPin); //Using Pin #10 (D7) to send 433mhz signals to screen
-  RF.setProtocol(1); //HomeGear Screen uses protocol 1 so set it
+  RF.enableTransmit(mhzPin); //Set RF transmit pin (D10)
+  RF.setProtocol(protocol); //Hset RF protocal
 
-  pinMode(ledPin, OUTPUT); //Set pin mode
-  pinMode(interruptPin, INPUT); //Set pin mode
+  pinMode(ledPin, OUTPUT); //Set pin mode to power for LED
+  pinMode(interruptPin, INPUT); //Set pin mode to input for interrupt pin
 
   attachInterrupt(digitalPinToInterrupt(interruptPin), move, CHANGE); //Wait for signal change from projector
 
-  delay(1000); //Give some time
+  delay(500); //Give some time
 
   move(); //Set state right away to make sure it operates properly
 }
@@ -41,11 +48,11 @@ void loop() {
 }
 
 void move() {
-  delay(1000); //Give pin time to stablalize
+  delay(500); //Give pin time to stablalize
 
   int status = digitalRead(interruptPin);
 
-  delay(1000); //We Read it now wait a second before we act
+  delay(500); //We Read it now wait a second before we act
 
   switch (status) {
     case LOW: //No Signal
--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # Send a 433mhz signal to a HomeGear projector screen from a 12v Projector trigger
-## Parts Required: (Amazon Affiliate links below, use the links to support me)
-* Arduino Nano - http://amzn.to/2FWJVos (requires special driver, see below)
+This was designed and coded to work for [Homegear 110” HD Motorized 16:9 Projector Screen W/ Remote Control](http://amzn.to/2ppoBB0). Codes in ProjectorScreen433mkz.ino are directly read from the remote that comes with the Homegear screen. If you purchase a screen other than [this one](http://amzn.to/2ppoBB0), you can still read the codes as long as you can verify your screen operates on 433mhz. This guide will explain how to read 433mhz codes and set them in the code.
+
+
+## Software Required
+* Arduino IDE - https://www.arduino.cc/en/Main/Software (Thank you Arduino)
+* CH340 Driver - http://bit.ly/2pMF4in (Thank you Elegoo) or use CH340 Driver.zip in this repo
+* RC Swtich Arduino Library - https://github.com/sui77/rc-switch/ (Thank you sui77)
+
+## Hardware Required: (Amazon Affiliate links below, use the links to support me)
+* Arduino Nano Clone - http://amzn.to/2FWJVos (requires special driver, see CH340 Driver above)
 * 433mhz kit - http://amzn.to/2FXDhhM (TX required RX if you need to read in new codes for your screen)
-* Misc wires and cables to complete install.
+* 12v to 3v - http://amzn.to/2DFVkHp (Nano can only take 5v. 3v is enough to trigger pin HIGH)
+* Misc wires and cables to complete install. (Mini B USB cable, 12v Trigger Wire from projector)
+
 
 ### Install Arduino IDE
-### Install CH Driver
+Head over to https://www.arduino.cc/en/Main/Software and install the Arduino IDE for your Operating System. Windows, MAC, Linux are all suported.
+
+### Install CH340 Driver
+Head over to http://bit.ly/2pMF4in or use CH340 Driver.zip from this repo.
+Select the files for your Operating System to install the Driver. This will allow the Arduino IDE to flash the Arduino Nano Clone above.
 ### Read Codes
 ### Upload Arduino Code to Nano
 ### Wire up 433mhz
